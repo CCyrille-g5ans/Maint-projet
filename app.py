3fc82ef96b145c8c120daa09023dbb79c34528df
Depot github CCyrille-g5ans <cyrille.sertlet@gmail.com>
2024-12-08 16:46:56 +0100

    V0.001

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+
+@app.route('/test/')
+def test():
+    return home() + " vous êtes sur "
+
+
 if __name__ == '__main__':
     app.run(debug=True)
